904250f0794bf8e35f66de5e14bd2c1439aec088
aayushjoshi992 <aayushj992@gmail.com>
2026-07-23 14:29:51 +0545

    Learned Python String

---
+++ Ch-1 PS/String.py
@@ -0,0 +1,9 @@
+str="This is my new new string from now onwards "
+print(len(str))
+print(str.count('o'))
+print(str.upper())
+print(str.lower())
+print(str.strip())
+print(str.capitalize())
+print(str.find("my"))
+print(str.replace("is","are"))
